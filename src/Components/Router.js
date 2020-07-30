1a21a0886b1b352afeeae5ccfb98e369c3569f5f
hshine1226 <hshine1226@gmail.com>
2020-07-31 00:29:53 +0900

    Add Switch on Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" exact component={TV} />
-    <Route path="/search" exact component={Search} />
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+        <Route path="/search" component={Search} />
+        {/* 일치하는 route가 없다면  Home으로 Redirect 시킨다.*/}
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
