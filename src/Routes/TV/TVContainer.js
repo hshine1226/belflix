a744ae11279f5994bf1673579763db8f4fd6c586
hshine1226 <hshine1226@gmail.com>
2020-08-01 17:02:33 +0900

    Add login of TV on Container

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -10,8 +11,36 @@ export default class extends React.Component {
     loading: true,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+
+      this.setState({
+        airingToday,
+        popular,
+        topRated,
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({ error: "TV 프로그램 정보를 찾을 수 없습니다." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, error, loading } = this.state;
+
     return (
       <TVPresenter
         topRated={topRated}
