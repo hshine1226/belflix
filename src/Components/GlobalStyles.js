220fe065e5b4382bf120216b4891cc138c5fe145
hshine1226 <hshine1226@gmail.com>
2020-08-01 19:55:37 +0900

    Add Korean font(Noto Sans KR) on GlobalStyles

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 const globalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
     ${reset};
     a{
         text-decoration: none;
@@ -10,7 +11,7 @@ const globalStyles = createGlobalStyle`
         box-sizing: border-box;
     }
     body{
-        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'Noto Sans KR', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 12px;
         background-color: rgba(20, 20, 20, 1);
         color: white;
