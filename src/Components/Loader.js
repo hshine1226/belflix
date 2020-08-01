36ca4bd256ffb21cc437e4356780394115068177
hshine1226 <hshine1226@gmail.com>
2020-08-01 21:05:36 +0900

    Add Loader component for showing load inform

---
+++ src/Components/Loader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  font-size: 28px;
+  margin-top: 22px;
+`;
+
+const Loader = () => <Container>Loadding...</Container>;
+
+export default Loader;
