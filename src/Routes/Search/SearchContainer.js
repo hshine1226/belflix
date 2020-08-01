e9b92251662af508c0e5242cfc7f4e0bbdcad2ae
hshine1226 <hshine1226@gmail.com>
2020-08-01 17:02:22 +0900

    Add login of Search on Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,17 +1,45 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
     movieResults: null,
     tvResults: null,
-    searchTrem: "",
+    searchTerm: "",
     error: null,
     loading: false,
   };
 
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+
+      this.setState({ movieResults, tvResults });
+    } catch {
+      this.setState({ error: "결과를 찾을 수 없습니다." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { error, loading, movieResults, tvResults, searchTrem } = this.state;
+    console.log(this.state);
     return (
       <SearchPresenter
         error={error}
@@ -19,6 +47,7 @@ export default class extends React.Component {
         movieResults={movieResults}
         tvResults={tvResults}
         searchTrem={searchTrem}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
