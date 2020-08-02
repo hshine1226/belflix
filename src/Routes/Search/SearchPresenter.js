fc9aac165bd8b2d449618edfa047c327c6bf5a36
hshine1226 <hshine1226@gmail.com>
2020-08-02 17:48:24 +0900

    Add Poster component on each Presenter

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Loader from "../../Components/Loader";
 import Section from "../../Components/Section";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
   padding: 0 20px;
@@ -44,14 +45,32 @@ const SearchPresenter = ({
         {movieResults && movieResults.length > 0 && (
           <Section title="Movie Results">
             {movieResults.map((movie) => (
-              <span key={movie.id}>{movie.title}</span>
+              <Poster
+                id={movie.id}
+                key={movie.id}
+                title={movie.title}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
             ))}
           </Section>
         )}
         {tvResults && tvResults.length > 0 && (
           <Section title="TV Show Results">
             {tvResults.map((show) => (
-              <span key={show.id}>{show.name}</span>
+              <Poster
+                id={show.id}
+                key={show.id}
+                title={show.name}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                year={
+                  show.first_air_date && show.first_air_date.substring(0, 4)
+                }
+                isMovie={false}
+              />
             ))}
           </Section>
         )}
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
   padding: 0 10px;
@@ -17,21 +18,45 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
           {topRated.map((show) => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              id={show.id}
+              key={show.id}
+              title={show.name}
+              imageUrl={show.poster_path}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+              isMovie={false}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Rated Shows">
           {popular.map((show) => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              id={show.id}
+              key={show.id}
+              title={show.name}
+              imageUrl={show.poster_path}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+              isMovie={false}
+            />
           ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today">
           {airingToday.map((show) => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              id={show.id}
+              key={show.id}
+              title={show.name}
+              imageUrl={show.poster_path}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+              isMovie={false}
+            />
           ))}
         </Section>
       )}
