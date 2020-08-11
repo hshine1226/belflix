26cbfdc1c34b49e8526de18dcc1c4fbab6aa5fb3
hshine1226 <hshine1226@gmail.com>
2020-08-11 11:01:50 +0900

    Set font size 12px to 14px

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -12,7 +12,7 @@ const globalStyles = createGlobalStyle`
     }
     body{
         font-family: 'Noto Sans KR', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size: 12px;
+        font-size: 14px;
         background-color: rgba(20, 20, 20, 1);
         color: white;
         padding-top: 50px;
