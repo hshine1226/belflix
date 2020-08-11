333947915f22c27946c0f5c755106af62acfcdfb
hshine1226 <hshine1226@gmail.com>
2020-08-12 00:34:15 +0900

    Add function loading another pages

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,23 +1,146 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
 import Poster from "../../Components/Poster";
+import { moviesApi } from "../../api";
 
 const Container = styled.div`
   padding: 20px;
 `;
 
-const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) =>
-  loading ? (
+const ButtonPrev = styled.a`
+  cursor: pointer;
+  position: absolute;
+  top: -34px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: auto;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 23px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
+
+const ButtonNext = styled.a`
+  cursor: pointer;
+  position: absolute;
+  bottom: -90px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: auto;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 23px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  border-radius: 3px 0 0 3px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) => {
+  const [nowPage, setNowPage] = useState(1);
+  const [popularPage, setPopularPage] = useState(1);
+  const [upcomingPage, setUpcomingPage] = useState(1);
+  const [nowPlayingState, setNowPlayingState] = useState(null);
+  const [popularState, setPopularState] = useState(null);
+  const [upcomingState, setUpcomingState] = useState(null);
+
+  useEffect(() => {
+    if (nowPlayingState === null) {
+      setNowPlayingState(nowPlaying);
+    }
+    if (popularState === null) {
+      setPopularState(popular);
+    }
+    if (upcomingState === null) {
+      setUpcomingState(upcoming);
+    }
+  }, [
+    nowPlaying,
+    popular,
+    upcoming,
+    nowPlayingState,
+    popularState,
+    upcomingState,
+  ]);
+
+  useEffect(() => {
+    moviesApi.nowPlaying(nowPage).then(({ data: { results } }) => {
+      setNowPlayingState(results);
+    });
+  }, [nowPage]);
+
+  useEffect(() => {
+    moviesApi.popular(popularPage).then(({ data: { results } }) => {
+      setPopularState(results);
+    });
+  }, [popularPage]);
+
+  useEffect(() => {
+    moviesApi.upcoming(upcomingPage).then(({ data: { results } }) => {
+      setUpcomingState(results);
+    });
+  }, [upcomingPage]);
+
+  const handleNowNext = async () => {
+    setNowPage(nowPage + 1);
+  };
+
+  const handleNowPrev = () => {
+    if (nowPage > 1) {
+      setNowPage(nowPage - 1);
+      moviesApi.nowPlaying(nowPage).then(({ data: { results } }) => {
+        setNowPlayingState(results);
+      });
+    }
+  };
+
+  const handlePopularNext = () => {
+    setPopularPage(popularPage + 1);
+  };
+
+  const handlePopularPrev = () => {
+    if (popularPage > 1) {
+      setPopularPage(popularPage - 1);
+      moviesApi.popular(popularPage).then(({ data: { results } }) => {
+        setPopularState(results);
+      });
+    }
+  };
+
+  const handleUpcomingNext = () => {
+    setUpcomingPage(upcomingPage + 1);
+  };
+
+  const handleUpcomingPrev = () => {
+    if (upcomingPage > 1) {
+      setUpcomingPage(upcomingPage - 1);
+      moviesApi.upcoming(upcomingPage).then(({ data: { results } }) => {
+        setUpcomingState(results);
+      });
+    }
+  };
+
+  return loading ? (
     <Loader />
   ) : (
     <Container>
-      {nowPlaying && nowPlaying.length > 0 && (
+      {nowPlayingState && nowPlayingState.length > 0 ? (
         <Section title="Now playing">
-          {nowPlaying.map((movie) => (
+          {nowPlayingState.map((movie) => (
             <Poster
               id={movie.id}
               key={movie.id}
@@ -28,11 +151,13 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) =>
               isMovie={true}
             />
           ))}
+          <ButtonPrev onClick={handleNowPrev}>&#x25B4;</ButtonPrev>
+          <ButtonNext onClick={handleNowNext}>&#x25BE;</ButtonNext>
         </Section>
-      )}
-      {upcoming && upcoming.length > 0 && (
+      ) : null}
+      {upcomingState && upcomingState.length > 0 && (
         <Section title="Upcoming Movies">
-          {upcoming.map((movie) => (
+          {upcomingState.map((movie) => (
             <Poster
               id={movie.id}
               key={movie.id}
@@ -43,11 +168,13 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) =>
               isMovie={true}
             />
           ))}
+          <ButtonPrev onClick={handleUpcomingPrev}>&#x25B4;</ButtonPrev>
+          <ButtonNext onClick={handleUpcomingNext}>&#x25BE;</ButtonNext>
         </Section>
       )}
-      {popular && popular.length > 0 && (
+      {popularState && popularState.length > 0 && (
         <Section title="Popular Movies">
-          {popular.map((movie) => (
+          {popularState.map((movie) => (
             <Poster
               id={movie.id}
               key={movie.id}
@@ -58,11 +185,14 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) =>
               isMovie={true}
             />
           ))}
+          <ButtonPrev onClick={handlePopularPrev}>&#x25B4;</ButtonPrev>
+          <ButtonNext onClick={handlePopularNext}>&#x25BE;</ButtonNext>
         </Section>
       )}
       {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
+};
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
