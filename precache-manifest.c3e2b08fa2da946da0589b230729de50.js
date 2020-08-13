066db2633c0db87ec5d8a69c3c83758f02f28f5d
hshine1226 <hshine1226@gmail.com>
2020-08-13 12:31:52 +0900

    Updates

---
+++ precache-manifest.c3e2b08fa2da946da0589b230729de50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f110ac546253b0a17230b995661dfefa",
+    "url": "/belflix/index.html"
+  },
+  {
+    "revision": "77e726c2cc1d208c43d2",
+    "url": "/belflix/static/js/2.22e1ac5a.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/belflix/static/js/2.22e1ac5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44050acdfc67bf07a7a2",
+    "url": "/belflix/static/js/main.fa42a513.chunk.js"
+  },
+  {
+    "revision": "d0108c9ea832ced9d7ea",
+    "url": "/belflix/static/js/runtime-main.daeb39da.js"
+  }
+]);
